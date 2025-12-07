839e409b7632952c0f6145a74e9454bae9d7da20
nastyarns1-tech <nastya.rns1@gmail.com>
2025-12-07 16:12:44 +0600

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function MonthPage() {
-  return (
-    <div className="stub-page">
-      <h1 className="page-title">Погода в Омске - Прогноз на сегодня</h1>
-    </div>
-  );
-}
--- app/week/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function MonthPage() {
-  return (
-    <div className="stub-page">
-      <h1 className="page-title">Погода в Омске - Прогноз на неделю</h1>
-    </div>
-  );
-}
